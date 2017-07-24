f5d02198c8a975d3250e51f08b07e7bd5813bb4c
indiajoe <indiajoe@gmail.com>
2017-07-25 02:10:44 +0530

    updated readme and setup.py to make installation easy

--- README.md
+++ README.md
@@ -1,8 +1,14 @@
-# HxRG_simulator
-Simulates output of Teledyne HxRG detectors
+pyHxRG
+-------
+Reduces or Simulates output of Teledyne HxRG detectors
 
-## Dependencies
-Noise generator code used is the following
+See `docs/` directory for examples
+
+## Dependencies for Reduction
+numpy
+
+## Dependencies for Simulator
+Noise generator code used is the following (Please cite them if simulator is used)
 + (Updated 11/6/15 to Rev. 2.6) Teledyne H1RG, H2RG, and H4RG Noise Generator, Publications of the Astronomical Society of the Pacific, by B.J. Rauscher, July 2015 
 Download .tar.gz software file and install from their [webpage](https://jwst.nasa.gov/publications.html)
 Reference paper: http://adsabs.harvard.edu/abs/2015PASP..127.1144R
---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+setup(name='pyHxRG',
+      version='0.1',
+      description='Python Tool for Reducing HxRG near-infrared data',
+      long_description = readme(),
+      classifiers=[
+          'License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)',
+          'Programming Language :: Python :: 2.7',
+          'Topic :: Scientific/Engineering :: Astronomy',
+      ],
+      keywords='H2RG HxRG NIR Astronomy Data Reduction',
+      url='https://github.com/indiajoe/pyHxRG',
+      author='Joe Ninan',
+      author_email='indiajoe@gmail.com',
+      license='LGPLv3+',
+      packages=['pyHxRG'],
+      install_requires=[
+          'numpy',
+      ],
+      include_package_data=True,
+      zip_safe=False)
