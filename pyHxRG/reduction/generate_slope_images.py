6636a7ef8dd0f607f13e7bac0683b25f54b0d5f0
Joe Philip Ninan <indiajoe@gmail.com>
2017-11-19 11:28:07 -0500

    Skip empty input directory

--- pyHxRG/reduction/generate_slope_images.py
+++ pyHxRG/reduction/generate_slope_images.py
@@ -382,6 +382,9 @@ def main_Teledyne():
         # Find the number of Ramps in the input Directory
         imagelist = sorted((os.path.join(InputDir,f) for f in os.listdir(InputDir) if (os.path.splitext(f)[-1] == '.fits')))
         RampList = sorted(set((int(re.search('H2RG_R(.+?)_M',os.path.basename(f)).group(1)) for f in imagelist))) # 45 in H2RG_R45_M01_N01.fits
+        if not RampList:
+            logging.info('No images to process in {0}'.format(InputDir))
+            continue # skip to next directory
         if args.NoNDR_Drop_G is None:
             noNDR,noDrop,noG = estimate_NoNDR_Drop_G_TeledyneData(imagelist)
         else:
